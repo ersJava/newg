aa2543de26d6c577e426080dfa6fa9828ee9227b
Beth <ersergeant@gmail.com>
2019-04-23 20:30:05 -0400

    adding layout and components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,16 @@
-import React from "react"
+import React from 'react'
 
-export default () => <div>Hello world!</div>
+import Header from '../components/header'
+import Footer from '../components/footer'
+
+const IndexPage = () => {
+    return (
+        <div>
+            <Header />
+            <h1>Trying this again. Practice is good.</h1>
+            <Footer />
+            </div>
+    )
+}
+
+export default IndexPage
